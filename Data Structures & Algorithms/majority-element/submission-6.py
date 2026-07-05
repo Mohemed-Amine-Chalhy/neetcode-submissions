db641fc51daec0adacb23c098cc76e0dc980d0be
Mohamed Amine CHALHY <107750540+Mohemed-Amine-Chalhy@users.noreply.github.com>
2026-07-05 14:33:41 +0100

    Add: majority-element - submission-6

---
+++ Data Structures & Algorithms/majority-element/submission-6.py
@@ -0,0 +1,21 @@
+#Boyer Moore Voting Algorithm
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = nums[0]
+        stamina = 0
+        for num in nums:
+            if stamina == 0:
+                candidate = num
+                
+            
+            if candidate == num:
+                stamina +=1 
+            else:
+                stamina -= 1
+
+
+        return candidate
+
+
+
+        
